00e04752257e6e23d93cd6076bae66e687d013be
Lev Novitskiy <levnovitskiy@gmail.com>
2023-05-27 18:58:58 +0300

    Optimized Floyd-Warshall - cycles merged, Edge search with dict

--- graphormer/functional.py
+++ graphormer/functional.py
@@ -40,9 +40,38 @@ def single_source_shortest_path(G, source, cutoff=None):
     return node_paths, edge_paths
 
 
+def fast_single_source_shortest_path(G, source, cutoff=None):
+    if source not in G:
+        raise nx.NodeNotFound("Source {} not in G".format(source))
+
+    edges = {edge: i for i, edge in enumerate(G.edges())}
+
+    level = 0  # the current level
+    nextlevel = {source: 1}  # list of nodes to check at next level
+    node_paths = {source: [source]}  # paths dictionary  (paths to key from source)
+    edge_paths = {source: []}
+
+    while nextlevel:
+        thislevel = nextlevel
+        nextlevel = {}
+        for v in thislevel:
+            for w in G[v]:
+                if w not in node_paths:
+                    node_paths[w] = node_paths[v] + [w]
+                    edge_paths[w] = edge_paths[v] + [edges[tuple(node_paths[w][-2:])]]
+                    nextlevel[w] = 1
+
+        level = level + 1
+
+        if (cutoff is not None and cutoff <= level):
+            break
+
+    return node_paths, edge_paths
+
+
 def all_pairs_shortest_path(G, cutoff=None) -> Tuple[Dict[int, List[int]], Dict[int, List[int]]]:
     # OPTIMIZE: This can be trivially parallelized.
-    paths = {n: single_source_shortest_path(G, n, cutoff=cutoff) for n in G}
+    paths = {n: fast_single_source_shortest_path(G, n, cutoff=cutoff) for n in G}
     node_paths = {n: paths[n][0] for n in paths}
     edge_paths = {n: paths[n][1] for n in paths}
     return node_paths, edge_paths
