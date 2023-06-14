c593a2de3d4a15487abfe60c8c88f096e392fbe1
Kirill Zakharov <kil4gen@gmail.com>
2023-06-14 17:09:29 +0300

    replace lists with pool.map

--- graphormer/functional.py
+++ graphormer/functional.py
@@ -1,4 +1,5 @@
 from typing import Tuple, Dict, List
+from multiprocessing import Pool
 
 import networkx as nx
 from torch_geometric.data import Data
@@ -6,29 +7,30 @@ from torch_geometric.utils.convert import to_networkx
 
 
 def floyd_warshall_source_to_all(G, source, cutoff=None):
+    # хочу typing
     if source not in G:
         raise nx.NodeNotFound("Source {} not in G".format(source))
 
     edges = {edge: i for i, edge in enumerate(G.edges())}
 
     level = 0  # the current level
-    nextlevel = {source: 1}  # list of nodes to check at next level
-    node_paths = {source: [source]}  # paths dictionary  (paths to key from source)
+    nextlevel = [source]  # list of nodes to check at next level
+    node_paths = {source: [source]}  # paths dictionary (paths to key from source)
     edge_paths = {source: []}
 
     while nextlevel:
         thislevel = nextlevel
-        nextlevel = {}
+        nextlevel = []
         for v in thislevel:
             for w in G[v]:
                 if w not in node_paths:
                     node_paths[w] = node_paths[v] + [w]
                     edge_paths[w] = edge_paths[v] + [edges[tuple(node_paths[w][-2:])]]
-                    nextlevel[w] = 1
+                    nextlevel.append(w)
 
         level = level + 1
 
-        if (cutoff is not None and cutoff <= level):
+        if cutoff is not None and cutoff <= level:
             break
 
     return node_paths, edge_paths
@@ -48,6 +50,7 @@ def shortest_path_distance(data: Data) -> Tuple[Dict[int, List[int]], Dict[int, 
 
 
 def batched_all_pairs_shortest_path(G) -> Tuple[Dict[int, List[int]], Dict[int, List[int]]]:
+    # same code as `all_pairs_shortest_path`?
     paths = {n: floyd_warshall_source_to_all(G, n) for n in G}
     node_paths = {n: paths[n][0] for n in paths}
     edge_paths = {n: paths[n][1] for n in paths}
@@ -55,19 +58,19 @@ def batched_all_pairs_shortest_path(G) -> Tuple[Dict[int, List[int]], Dict[int, 
 
 
 def batched_shortest_path_distance(data) -> Tuple[Dict[int, List[int]], Dict[int, List[int]]]:
-    graphs = [to_networkx(sub_data) for sub_data in data.to_data_list()]
+    # type(Data) == Batch?
+    pool = Pool()
     relabeled_graphs = []
     shift = 0
-    for i in range(len(graphs)):
-        num_nodes = graphs[i].number_of_nodes()
-        relabeled_graphs.append(nx.relabel_nodes(graphs[i], {i: i + shift for i in range(num_nodes)}))
+    for graph in pool.map(to_networkx, data.to_data_list()):
+        num_nodes = graph.number_of_nodes()
+        relabeled_graphs.append(nx.relabel_nodes(graph, {i: i + shift for i in range(num_nodes)}))
         shift += num_nodes
 
-    paths = [batched_all_pairs_shortest_path(G) for G in relabeled_graphs]
     node_paths = {}
     edge_paths = {}
 
-    for path in paths:
+    for path in pool.map(batched_all_pairs_shortest_path, relabeled_graphs):
         for k, v in path[0].items():
             node_paths[k] = v
         for k, v in path[1].items():
