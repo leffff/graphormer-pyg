f9afda1f0de7921c4b7cfb831293442130ee6eb0
Kirill Zakharov <kil4gen@gmail.com>
2023-06-14 17:36:57 +0300

    added type hints for graphs

--- graphormer/functional.py
+++ graphormer/functional.py
@@ -2,12 +2,11 @@ from typing import Tuple, Dict, List
 from multiprocessing import Pool
 
 import networkx as nx
-from torch_geometric.data import Data
+from torch_geometric.data import Data, Batch
 from torch_geometric.utils.convert import to_networkx
 
 
-def floyd_warshall_source_to_all(G, source, cutoff=None):
-    # хочу typing
+def floyd_warshall_source_to_all(G: nx.DiGraph|nx.Graph, source, cutoff=None):
     if source not in G:
         raise nx.NodeNotFound("Source {} not in G".format(source))
 
@@ -36,7 +35,7 @@ def floyd_warshall_source_to_all(G, source, cutoff=None):
     return node_paths, edge_paths
 
 
-def all_pairs_shortest_path(G) -> Tuple[Dict[int, List[int]], Dict[int, List[int]]]:
+def all_pairs_shortest_path(G: nx.DiGraph|nx.Graph) -> Tuple[Dict[int, List[int]], Dict[int, List[int]]]:
     paths = {n: floyd_warshall_source_to_all(G, n) for n in G}
     node_paths = {n: paths[n][0] for n in paths}
     edge_paths = {n: paths[n][1] for n in paths}
@@ -45,24 +44,29 @@ def all_pairs_shortest_path(G) -> Tuple[Dict[int, List[int]], Dict[int, List[int
 
 def shortest_path_distance(data: Data) -> Tuple[Dict[int, List[int]], Dict[int, List[int]]]:
     G = to_networkx(data)
+    # from to_networkx docs: returns networkx.Graph if to_undirected is set to True,
+    # or a directed networkx.DiGraph otherwise
+    # т.ч. я типизацию такую прикручиваю
     node_paths, edge_paths = all_pairs_shortest_path(G)
     return node_paths, edge_paths
 
 
-def batched_all_pairs_shortest_path(G) -> Tuple[Dict[int, List[int]], Dict[int, List[int]]]:
-    # same code as `all_pairs_shortest_path`?
+def batched_all_pairs_shortest_path(G: nx.DiGraph|nx.Graph) -> Tuple[Dict[int, List[int]], Dict[int, List[int]]]:
+    # exactly the same code as `all_pairs_shortest_path`?
     paths = {n: floyd_warshall_source_to_all(G, n) for n in G}
     node_paths = {n: paths[n][0] for n in paths}
     edge_paths = {n: paths[n][1] for n in paths}
     return node_paths, edge_paths
 
 
-def batched_shortest_path_distance(data) -> Tuple[Dict[int, List[int]], Dict[int, List[int]]]:
-    # type(Data) == Batch?
+def batched_shortest_path_distance(data: Batch) -> Tuple[Dict[int, List[int]], Dict[int, List[int]]]:
+    # судя по `data.to_data_list()` type(Data) == Batch?
     pool = Pool()
     relabeled_graphs = []
     shift = 0
     for graph in pool.map(to_networkx, data.to_data_list()):
+        # Expected type 'Iterable[Data]' (matched generic type 'Iterable[_S]'), got 'list[BaseData]' instead
+        # Норм warning?
         num_nodes = graph.number_of_nodes()
         relabeled_graphs.append(nx.relabel_nodes(graph, {i: i + shift for i in range(num_nodes)}))
         shift += num_nodes
